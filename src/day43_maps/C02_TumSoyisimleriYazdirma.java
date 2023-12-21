210a03e6433926112dca48ed8e1c4a5d1e85072f
İsmail Kılıç <kaunistr@gmail.com>
2023-12-22 00:24:02 +0300

    23.12.21

--- src/day43_maps/C02_TumSoyisimleriYazdirma.java
+++ src/day43_maps/C02_TumSoyisimleriYazdirma.java
@@ -1,62 +1,44 @@
 package day43_maps;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-public class C01_MapOlusturmaElementEkleme {
+public class C02_TumSoyisimleriYazdirma {
 
     public static void main(String[] args) {
 
-        /*
-            Bir ogrenciyi map'e eklemek icin put(key,value) kullaniriz
+        Map<Integer,String> ogrenciMap = new HashMap<>();
 
-            bir ogrencinin value'sune ulasmak isterseniz
-            - ogrenciMap.get(ogrenciNo) ile String olarak tum value'yu alip kaydederiz
-            - value, pekcok bilgi barindirdigindan, her bir bilgiye ulasmak icin
-               value.split("-") ile value'yu array'e ceviririz
-            - array'deki tum bilgilerin index'lerine sahip oldugumuzdan
-              istenen bilgileri array'den alip yazdirabilir veya kullanabiliriz
+        ogrenciMap.put(101,"Ali-Can-11-H-MF");
+        ogrenciMap.put(102,"Veli-Cem-10-K-TM");
+        ogrenciMap.put(103,"Ali-Cem-11-K-TM");
+        ogrenciMap.put(104,"Ayse-Can-10-H-MF");
+        ogrenciMap.put(105,"Sevgi-Cem-11-M-TM");
+        ogrenciMap.put(106,"Sevgi-Can-10-K-MF");
+        ogrenciMap.put(107,"Esra-Han-11-M-SOZ");
+        ogrenciMap.put(108,"Azim-Kan-12-L-SOZ");
+        ogrenciMap.put(109,"Huseyin-Fan-12-H-MF");
+        ogrenciMap.put(110,"Azim-Kayisi-11-K-TM");
 
+        // 106 numarali ogrencinin soyisimini yazdirin
 
+        // once 106 numarali ogrencinin value'sune ulasip kaydedelim
 
-         */
-
-        Map<Integer, String> ogrenciMap = new HashMap<>();
-        ogrenciMap.put(101,"Ali-Can-10-K-MF");
-        // bu ogrenci icin key : 101
-        //               value : "Ali-Can-10-K-MF"
-        // value String olmak zorunda degil list, array, Integer, Map, Queue
-
-        String value101 = ogrenciMap.get(101);
-
-        System.out.println("101'in tum bilgisi : " + value101); // Ali-Can-10-K-MF
+        // String value106 = ogrenciMap.get(106); // Sevgi-Can-10-K-MF
+        // String[] value106Arr = value106.split("-"); //  [Sevgi, Can, 10, K, MF]
+        // System.out.println("106 soyisim : " + value106Arr[1]); // 106 soyisim : Can
 
 
-        // 101 numarali ogrencinin soyismini yazdirin
+        // Tum ogrencilerin soyisimlerini yazdirin
+        Set<Integer> ogrenciKeySeti = ogrenciMap.keySet(); // [101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
 
-        String[] ogrenci101BilgilerArr = value101.split("-");
-
-        System.out.println(Arrays.toString(ogrenci101BilgilerArr));
-        // [Ali, Can, 10, K, MF]
-
-
-        System.out.println("101 numarali ogrenci soyismi : " + ogrenci101BilgilerArr[1]); // Can
-
-        // 101 numarali ogrencinin bolumunu yazdirin
-
-        System.out.println("101 numarali ogrenci bolumu : " + ogrenci101BilgilerArr[4]); // MF
-
-        // 101 numarali ogrencinin isim ve soyismini yazdirin
-
-        System.out.println("101 numarali ogrencinin isim ve soyismi : " +
-                ogrenci101BilgilerArr[0]+ " "+ ogrenci101BilgilerArr[1]); // Ali Can
-
-
-        // 101 numarali ogrencinin isim ve soyismini yazdirin, soyisim buyuk harf olarak yazdirilsin
-
-        System.out.println("101 numarali ogrencinin isim ve soyismi : " +
-                ogrenci101BilgilerArr[0]+ " "+ ogrenci101BilgilerArr[1].toUpperCase()); // Ali CAN
+        for (Integer eachKey: ogrenciKeySeti
+        ) {
+            String valueEach = ogrenciMap.get(eachKey); // Sevgi-Can-10-K-MF
+            String[] valueEachArr = valueEach.split("-"); // //  [Sevgi, Can, 10, K, MF]
+            System.out.println(eachKey + " nolu ogrencinin soyismi : " + valueEachArr[1]);
+        }
 
     }
 }
